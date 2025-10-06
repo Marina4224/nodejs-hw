8e08dfeb544f5c78b058a0e2df5016a6d0730744
Marina <marinka1984mmm@gmail.com>
2025-10-06 21:36:49 +0300

    upDate controller

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -10,7 +10,7 @@ export const getAllNotes = async (req, res) => {
 // Отримати одного студента за id
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await note.findById(noteId);
+  const note = await Note.findById(noteId);
 
   if (!note) {
     next(createHttpError(404, 'Note not found'));
@@ -21,13 +21,13 @@ export const getNoteById = async (req, res, next) => {
 };
 
 export const createNote = async (req, res) => {
-  const note = await note.create(req.body);
+  const note = await Note.create(req.body);
   res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await note.findOneAndDelete({
+  const note = await Note.findOneAndDelete({
     _id: noteId,
   });
   if (!note) {
@@ -39,7 +39,7 @@ export const deleteNote = async (req, res, next) => {
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await note.findOneAndUpdate(
+  const note = await Note.findOneAndUpdate(
     { _id: noteId },
     req.body,
     { new: true },
