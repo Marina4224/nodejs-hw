82fa9d2a0d0ef83cc000c701c9e7c07a08a4c762
Marina <marinka1984mmm@gmail.com>
2025-10-24 20:40:52 +0300

    add avatar

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from "./middleware/errorHandler.js";
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 import cookieParser from "cookie-parser";
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(logger);
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,14 +1,26 @@
-import nodemailer from 'nodemailer';
+import { Readable } from 'node:stream';
+import { v2 as cloudinary } from 'cloudinary';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
-};
+export async function saveFileToCloudinary(buffer) {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'students-app/avatars',
+        resource_type: 'image',
+        overwrite: true,
+        unique_filename: true,
+        use_filename: false,
+      },
+      (err, result) => (err ? reject(err) : resolve(result)),
+    );
+
+    Readable.from(buffer).pipe(uploadStream);
+  });
+}
